4d3cb2dc081e749e352b41105f58812189bc7760
Asadulla <asadullas@propay.ai>
2026-05-24 11:58:41 +0530

    task manager

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected Successfully...');
-  } catch (err) {
-    console.error('Database connection failed:', err.message);
+    // 🚀 Backup Bypass: Agar undefined milega, toh direct string use karega
+    const dbURI = process.env.MONGO_URI || "mongodb+srv://shaikhasadulla77_db_user:HnfgA5qnGoqRXlKQ@cluster0.vfur3wl.mongodb.net/task-manager?retryWrites=true&w=majority";
+
+    const conn = await mongoose.connect(dbURI);
+    console.log("MongoDB Connected Successfully...");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
     process.exit(1); 
   }
 };
